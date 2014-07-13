a29aeef04827ff1faf48e3b45f4743ae2744ef5e
bmirtich <bmirtich@users.noreply.github.com>
2014-07-12 17:40:44 -0700

    more initial configuration changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ set :deploy_via, :remote_cache
 # Easier to do system level config as root - probably should do it through
 # sudo in the future.  We use ssh keys for access, so no passwd needed
 set :user, 'root'
+#set :user, 'ec2-user'
 set :password, nil
 
 # Use sudo with user rails for cap deploy:[stop|start|restart]
